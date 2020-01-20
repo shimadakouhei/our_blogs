cef48b9607d2fe50bcf819578b302f1a90ab6187
shimadakouhei <shimapykkm@gmail.com>
2020-01-20 19:11:06 +0900

    完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user)
   end  
 
   def new
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def create
+    # binding.pry
     Post.create(post_params)
     redirect_to root_path
   end
@@ -34,6 +35,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
   end  
 end
